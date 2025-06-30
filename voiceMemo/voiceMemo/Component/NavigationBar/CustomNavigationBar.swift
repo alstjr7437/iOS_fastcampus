51a9009f1b7f3c3cdf6b801a3cff76bd3e4dcd39
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-01 00:31:28 +0900

    feat: 커스텀 네비게이션바 구현

--- voiceMemo/voiceMemo/Component/NavigationBar/CustomNavigationBar.swift
+++ voiceMemo/voiceMemo/Component/NavigationBar/CustomNavigationBar.swift
@@ -6,13 +6,58 @@
 import SwiftUI
 
 struct CustomNavigationBar: View {
-  var body: some View {
-    Text("CustomNavigationBar")
-  }
+    let isDisplayLeftButton: Bool
+    let isDisplayRightButton: Bool
+    let leftButtonAction: () -> Void
+    let rightButtonAction: () -> Void
+    let rightButtonType: NavigationButtonType
+    
+    init(
+        isDisplayLeftButton: Bool = true,
+        isDisplayRightButtom: Bool = true,
+        leftButtonAction: @escaping () -> Void = {},
+        rightButtonAction: @escaping () -> Void = {},
+        rightButtonType: NavigationButtonType = .edit
+    ) {
+        self.isDisplayLeftButton = isDisplayLeftButton
+        self.isDisplayRightButton = isDisplayRightButtom
+        self.leftButtonAction = leftButtonAction
+        self.rightButtonAction = rightButtonAction
+        self.rightButtonType = rightButtonType
+    }
+    
+    var body: some View {
+        HStack {
+            if isDisplayLeftButton {
+                Button (
+                    action: leftButtonAction,
+                    label: { Image("leftArrow") }
+                )
+            }
+            
+            Spacer()
+            
+            if isDisplayRightButton {
+                Button(
+                    action: rightButtonAction,
+                    label: {
+                        if rightButtonType == .close {
+                            Image("close")
+                        } else {
+                            Text(rightButtonType.rawValue)
+                                .foregroundColor(.customBlack)
+                        }
+                    }
+                )
+            }
+        }
+        .padding(.horizontal, 20)
+        .frame(height: 20)
+    }
 }
 
 struct CustomNavigationBar_Previews: PreviewProvider {
-  static var previews: some View {
-    CustomNavigationBar()
-  }
+    static var previews: some View {
+        CustomNavigationBar()
+    }
 }
