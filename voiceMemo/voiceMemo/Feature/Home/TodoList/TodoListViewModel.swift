9a1a388db60f87d685a884505dfec6734adefe81
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-01 00:48:41 +0900

    feat: TodoListViewModel 구현

--- voiceMemo/voiceMemo/Feature/Home/TodoList/TodoListViewModel.swift
+++ voiceMemo/voiceMemo/Feature/Home/TodoList/TodoListViewModel.swift
@@ -5,6 +5,71 @@
 
 import Foundation
 
-class TodoListViewModel {
-  
+final class TodoListViewModel: ObservableObject {
+    @Published var todos: [Todo]
+    @Published var isEditMode: Bool
+    @Published var removeTodos: [Todo]
+    @Published var isDisplayRemoveTodoAlert: Bool
+    
+    var removeTodosCount: Int { removeTodos.count }
+    
+    var navigationBarRightButtonMode: NavigationButtonType {
+        isEditMode ? .complete : .edit
+    }
+    
+    init(
+        todos: [Todo] = [],
+        isEditMode: Bool = false,
+        removeTodos: [Todo] = [],
+        isDisplayRemoveTodoAlert: Bool = false
+    ) {
+        self.todos = todos
+        self.isEditMode = isEditMode
+        self.removeTodos = removeTodos
+        self.isDisplayRemoveTodoAlert = isDisplayRemoveTodoAlert
+    }
+}
+
+extension TodoListViewModel {
+    func selectedBoxTapped(_ todo: Todo) {
+        if let index = todos.firstIndex(where: { $0 == todo}) {
+            todos[index].selected.toggle()
+        }
+    }
+    
+    func addTodo(_ todo: Todo) {
+        todos.append(todo)
+    }
+    
+    func navigationRightButtonTapped() {
+        if isEditMode {
+            if removeTodos.isEmpty {
+                isEditMode = false
+            } else {
+                // Alert
+            }
+        } else {
+            isEditMode = true
+        }
+    }
+    
+    func setIsDisplayRemoveTodoAlert(_ isDisplay: Bool) {
+        isDisplayRemoveTodoAlert = isDisplay
+    }
+    
+    func todoRemoveSelectedBoxTapped(_ todo: Todo){
+        if let index = removeTodos.firstIndex(of: todo) {
+            removeTodos.remove(at: index)
+        } else {
+            removeTodos.append(todo)
+        }
+    }
+    
+    func removeButtonTapped() {
+        todos.removeAll { todo in
+            removeTodos.contains(todo)
+        }
+        removeTodos.removeAll()
+        isEditMode = false
+    }
 }
--- voiceMemo/voiceMemo/Model/Todo/Todo.swift
+++ voiceMemo/voiceMemo/Model/Todo/Todo.swift
@@ -5,7 +5,7 @@
 
 import Foundation
 
-struct Todo {
+struct Todo: Hashable {
     var title: String
     var time: Date
     var day: Date
